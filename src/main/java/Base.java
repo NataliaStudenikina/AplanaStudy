3b554577fc7ba2e303bf0481acc6dcbb9cecd005
Natasha Studenikina <studenikina.n.a@gmail.com>
2018-07-12 23:54:59 +0300

    init

---
+++ src/main/java/Base.java
@@ -0,0 +1,9 @@
+/**
+ *Создание проекта в рамках обучения курсу "Java для тестировщиков 2.0" 11.07.2018
+ */
+public class Base {
+    public static void main(String[] args){
+        //Простой консольный вывод текста "Hello, world!"
+        System.out.println("Hello, world!");
+    }
+}
